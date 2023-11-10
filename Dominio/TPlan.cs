61f351c0ac1b469830fb33c4bf7e446764931add
LeoZidonis <leitozidonis@gmail.com>
2023-11-10 12:55:36 -0300

    agregado plannegocio

--- Dominio/TPlan.cs
+++ Dominio/TPlan.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class TPlan
+    public class TPlan
     {
         public int IdPlan { get; set; }
         public int CantidadMegas { get; set; }
