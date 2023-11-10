b2a397f2091f1c629b7a1cd5c21263930e2741d4
Teo Beltran <teo.belnan@gmail.com>
2023-11-09 23:54:07 -0300

    Database

--- NetPulse_DB.sql
+++ NetPulse_DB.sql
@@ -1,108 +1,187 @@
--- Crear la base de datos
-CREATE DATABASE BD_SXA
-GO
--- Usar la base de datos
-USE BD_SXA
-GO
-drop table clientes
--- Tabla de clientes
-CREATE TABLE clientes (
-    nroCliente INT PRIMARY KEY,
+CREATE DATABASE BD_NetPulse
+Go
+
+USE BD_NetPulse
+Go
+
+CREATE TABLE Clientes (
+    IdCliente INT PRIMARY KEY identity (1, 1),
     Nombre VARCHAR(255) NOT NULL,
+    Telefono VARCHAR(20),
+    Mail VARCHAR(255),
+    Dni VARCHAR(20) NOT NULL,
+    FechaAlta DATETIME,
+    Activo bit,
+)
+Go
+
+Create Table Domicilio(
+    IdDomicilio int primary key identity (1, 1),
     Direccion VARCHAR(255),
-    Barrio VARCHAR(255),
-    telefono VARCHAR(20),
-    mail VARCHAR(255),
-    dni VARCHAR(20) NOT NULL,
-    fechaAlta DATETIME,
-    formaPago VARCHAR(50),
-    activo bit,
-    nroPlan INT,
-    FOREIGN KEY (nroPlan) REFERENCES planes(nroPlan)
+    Barrio varchar(255),
+    Ciudad varchar(255),
+    Comentario varchar(255)
 )
-go
--- Tabla de planes
-CREATE TABLE planes (
-    nroPlan INT PRIMARY KEY,
-    cantMegas VARCHAR(50),
-    precio DECIMAL(10, 2),
-    activo bit
-)
-go
--- Tabla de abonos mensuales
-CREATE TABLE abonos_mensuales (
-    idAbono INT PRIMARY KEY,
-    nroCliente INT,
-    fechaAbono DATE,
-    monto DECIMAL(10, 2),
-    estadoPago VARCHAR(20),
-    formaPago VARCHAR(50),
-    -- Otros campos relevantes
-    FOREIGN KEY (nroCliente) REFERENCES clientes(nroCliente)
-)
-go
--- Tabla de instalaciones
-CREATE TABLE instalaciones (
-    idInstalacion INT PRIMARY KEY,
-    nroCliente INT,
-    fechaInstalacion DATE,
-    tecnicoResponsable VARCHAR(255),
-    componentesInstalados TEXT,
-    comentarios TEXT,
-    estadoRealizacion VARCHAR(50),
-    FOREIGN KEY (nroCliente) REFERENCES clientes(nroCliente)
-)
-go
--- Tabla de servicios realizados
-CREATE TABLE servicios_realizados (
-    idServicio INT PRIMARY KEY,
-    nroCliente INT,
-    fechaServicio DATE,
-    tecnicoResponsable VARCHAR(255),
-    descripcionServicio TEXT,
-    costo DECIMAL(10, 2),
-    componentesCambiados TEXT,
-    comentarios TEXT,
-    estadoRealizacion VARCHAR(50),
-    FOREIGN KEY (nroCliente) REFERENCES clientes(nroCliente)
-)
-GO
+Go
 
--- Tabla de desinstalaciones
-CREATE TABLE desinstalaciones (
-    idDesinstalacion INT PRIMARY KEY,
-    nroCliente INT,
-    fechaDesinstalacion DATE,
-    tecnicoResponsable VARCHAR(255),
-    razonDesinstalacion TEXT,
-    componentesRetirados TEXT,
-    comentarios TEXT,
-    estadoRealizacion VARCHAR(50),
-    FOREIGN KEY (nroCliente) REFERENCES clientes(nroCliente)
+Create Table TPlan(
+    IdPlan INT PRIMARY KEY identity (1, 1),
+    CantidadMegas int,
+    Precio money
 )
+Go
 
+Create Table FormaPago(
+    IdFormaPago int primary key identity (1, 1),
+    Nombre varchar(50)
+)
+Go
 
+Create Table AbonoMensual(
+    IdAbonoMensual int primary key identity (1, 1),
+    IdFormaPago int,
+    FechaVencimiento1 datetime,
+    FechaVencimiento2 datetime,
+    Pagado bit,
+    -- //////////////////
+    FOREIGN KEY (IdFormaPago) REFERENCES FormaPago (IdFormaPago)
+)
+Go
 
+CREATE TABLE Servicio (
+    IdServicio INT PRIMARY KEY identity (1, 1),
+    IdCliente INT,
+    IdDomicilio int,
+    IdPlan int,
+    IdAbonoMensual int,
+    FechaAlta datetime,
+    Estado bit,
+    --TecnicoResponsable VARCHAR(255),
+    Comentarios text,
+    FOREIGN KEY (IdCliente) REFERENCES Clientes (IdCliente),
+    FOREIGN KEY (IdDomicilio) REFERENCES Domicilio (IdDomicilio),
+    FOREIGN KEY (IdPlan) REFERENCES TPlan (IdPlan),
+    FOREIGN KEY (IdAbonoMensual) REFERENCES AbonoMensual (IdAbonoMensual)
+)
+Go
+Create Table Tecnico(
+    IdTecnico int PRIMARY KEY identity (1, 1),
+    Nombre VARCHAR(50),
+    Contacto VARCHAR(50),
+    FechaIncorporacion datetime,
+    Estado bit
+)
+Go
+Create Table TipoMantenimiento(
+    IdTipoMantenimiento int primary key identity (1, 1),
+    Nombre varchar(50)
+)
+Go
 
-INSERT INTO clientes (nroCliente, Nombre, Direccion, Barrio, telefono, mail, dni, fechaAlta, formaPago,activo,nroPlan)
+CREATE TABLE Mantenimiento (
+    IdMantenimiento int primary key identity (1, 1),
+    IdServicio int,
+    Fecha DATETIME,
+    IdTecnico int,
+    Descripcion text,
+    --Costo REVISAR
+    IdTipoMantenimiento int,
+    --ComponentesCambiados text,
+    Comentarios text,
+    EstadoRealizacion bit,
+    FOREIGN KEY (IdServicio) REFERENCES Servicio (IdServicio),
+    FOREIGN KEY (IdTecnico) REFERENCES Tecnico (IdTecnico),
+    FOREIGN KEY (IdTipoMantenimiento) REFERENCES TipoMantenimiento (IdTipoMantenimiento),
+)
+Go
+
+INSERT INTO Clientes (Nombre, Telefono, Mail, Dni, FechaAlta, Activo)
 VALUES
-(1170, 'Cabrera Hector Miguel', 'Ciudad parque las rosas manzana 38 casa 12', 'PLR', '3517 60-4237', 'Cabreraceleste974@gmail.com', '31557702', '2023-07-28', 'T',1,2),
-(1171, 'Alexis maximiliano Argüello Quiroga', 'San Marino 3875 o ( lote 2 manzana 3).', 'PLR', '3516 20-9714', 'alexmax744@gmail.com', '34316716', '2023-07-11', 'T',1,3),
-(1172, 'Gloria Jimena Salas', 'Cambay 5370  Barrio Carrara', 'CA', '3512 95-1064', 'gloriajsalas22@hotmail.com', '30330008', '2023-08-28', 'T',1,1),
-(1173, 'Hugo Andres Flores', 'Bonn 4385 Barrio Estación Flores', 'EF', '3518 12-5879', 'hugoandresf9@gmail.com', '14409678', '2023-08-31', 'T',1,1),
-(1174, 'Maximiliano jeremías villada', 'Roque arias 3618 barrio estación flores', 'EF', '3517 60-8633', 'achivillada@gmail.com', '33752252', '2023-08-07', 'T',1,1),
-(1175, 'Julieta Gabriela arce', 'Parque las rosas manzana 46 casa 6', 'PLR', '3517 43-2707', 'Hanseltian2327@gmail.com', '32787061', '2023-07-27', 'T',1,2),
-(1176, 'Verónica Isabel Escudero', 'María Brizuela 3137 Barrio Villa Aspacia', 'VA', '3513 47-4867', 'verisabel3007@gmail.com', '25608264', '2023-09-15', 'T',1,2),
-(1177, 'Jaquelina emilce tosi sanchez', 'Henderson 5467 carrara', 'CA', '3517 73-4013', 'Tosiestudio@gmail.com', '40521639', '2023-09-19', 'T',1,3),
-(1178, 'Julio Nicolás Vildoza Maltés', 'https://www.google.com/maps/dir/-31.4510948,-64.2395311/@-31.4510948,-64.242106,17z?hl=es&entry=ttu', 'PLR', '3512 48-5720', 'julio.nicolas31667@gmail.com', '31667694', '2023-08-02', 'T',1,1),
-(1179, 'Rocio Belen Arguello', 'calle pública sin numero', 'EF', '3517 53-3359', 'rociobelen52@outlook.com', '42585304', '2023-09-25', 'T',1,3),
-(1180, 'Carlos Ramón Nieto', 'B° parque las rosas mza44 csa21', 'PLR', '3517 50-5877', 'n23carlos@gmail.com', '17530865', '2023-09-21', 'T',1,3),
-(1181, 'Andrés Rolando González', 'Psj 52 casa 63, Villa Aspacia', 'VA', '3517 32-0353', 'andy40684909@gmail.com', '40684909', '2023-10-05', 'T',1,2),
-(1182, 'Díaz Brenda Aldana', 'Ciudad de nieva 3858', 'CA', '3513 39-2543', 'brendiaz40402@gmail.com', '40402959', '2023-10-05', 'T',1,3),
-(1183, 'Angela Lorena Tosi Sánchez', 'El alto 3849', 'SA', '3512 09-2093', 'lore1947@outlook.es', '1', '2023-10-05', 'T',1,3),
-(1184, 'Gisel Sosa', 'San Marino 45', 'PLR', '3513 68-0839', 'sosagise32@gmail.com', '39080827', '2023-10-11', 'T',1,2),
-(1185, 'Yanina Alejandra Bazan', 'Parque las Rosas mza:4 Lote:28', 'PLR', '3516 87-1920', 'yaninaariel34@gmail.com', '32292560', '2023-10-11', 'T',1,1);
+    ('Cliente1', '123-456-7890', 'cliente1@example.com', '123456789', GETDATE(), 1),
+    ('Cliente2', '987-654-3210', 'cliente2@example.com', '987654321', GETDATE(), 1),
+    ('Cliente3', '555-555-5555', 'cliente3@example.com', '555555555', GETDATE(), 1),
+    ('Cliente4', '111-111-1111', 'cliente4@example.com', '111111111', GETDATE(), 1),
+    ('Cliente5', '222-222-2222', 'cliente5@example.com', '222222222', GETDATE(), 1),
+    ('Cliente6', '333-333-3333', 'cliente6@example.com', '333333333', GETDATE(), 1),
+    ('Cliente7', '444-444-4444', 'cliente7@example.com', '444444444', GETDATE(), 1),
+    ('Cliente8', '666-666-6666', 'cliente8@example.com', '666666666', GETDATE(), 1),
+    ('Cliente9', '777-777-7777', 'cliente9@example.com', '777777777', GETDATE(), 1),
+    ('Cliente10', '999-999-9999', 'cliente10@example.com', '999999999', GETDATE(), 1);
 
+INSERT INTO Domicilio (Direccion, Barrio, Ciudad, Comentario)
+VALUES
+    ('Calle 1, Nº 123', 'Barrio A', 'Ciudad X', 'Comentario 1'),
+    ('Calle 2, Nº 456', 'Barrio B', 'Ciudad Y', 'Comentario 2'),
+    ('Calle 3, Nº 789', 'Barrio C', 'Ciudad Z', 'Comentario 3'),
+    ('Calle 4, Nº 1011', 'Barrio D', 'Ciudad W', 'Comentario 4'),
+    ('Calle 5, Nº 1213', 'Barrio E', 'Ciudad V', 'Comentario 5'),
+    ('Calle 6, Nº 1415', 'Barrio F', 'Ciudad U', 'Comentario 6'),
+    ('Calle 7, Nº 1617', 'Barrio G', 'Ciudad T', 'Comentario 7'),
+    ('Calle 8, Nº 1819', 'Barrio H', 'Ciudad S', 'Comentario 8'),
+    ('Calle 9, Nº 2021', 'Barrio I', 'Ciudad R', 'Comentario 9'),
+    ('Calle 10, Nº 2223', 'Barrio J', 'Ciudad Q', 'Comentario 10');
 
-INSERT into planes (nroPlan,precio,cantMegas,activo)
-VALUES(1,4100,12,1),(2,5200,18,1),(3,6600,25,1)
+INSERT INTO TPlan (CantidadMegas, Precio)
+VALUES
+    (10, 29.99),
+    (25, 49.99),
+    (50, 69.99),
+    (100, 89.99);
+
+INSERT INTO FormaPago (Nombre)
+VALUES
+    ('Tarjeta de Crédito'),
+    ('Transferencia Bancaria'),
+    ('Efectivo');
+
+INSERT INTO AbonoMensual (IdFormaPago, FechaVencimiento1, FechaVencimiento2, Pagado)
+VALUES
+    (1, DATEADD(MONTH, 1, GETDATE()), DATEADD(MONTH, 2, GETDATE()), 0),
+    (1, DATEADD(MONTH, 1, GETDATE()), DATEADD(MONTH, 2, GETDATE()), 1),
+    (1, DATEADD(MONTH, 1, GETDATE()), DATEADD(MONTH, 2, GETDATE()), 0),
+    (1, DATEADD(MONTH, 1, GETDATE()), DATEADD(MONTH, 2, GETDATE()), 1),
+    (1, DATEADD(MONTH, 1, GETDATE()), DATEADD(MONTH, 2, GETDATE()), 0),
+    (1, DATEADD(MONTH, 1, GETDATE()), DATEADD(MONTH, 2, GETDATE()), 1),
+    (1, DATEADD(MONTH, 1, GETDATE()), DATEADD(MONTH, 2, GETDATE()), 0),
+    (1, DATEADD(MONTH, 1, GETDATE()), DATEADD(MONTH, 2, GETDATE()), 1),
+    (1, DATEADD(MONTH, 1, GETDATE()), DATEADD(MONTH, 2, GETDATE()), 0),
+    (1, DATEADD(MONTH, 1, GETDATE()), DATEADD(MONTH, 2, GETDATE()), 1);
+
+INSERT INTO Servicio (IdCliente, IdDomicilio, IdPlan, IdAbonoMensual, FechaAlta, Estado, Comentarios)
+VALUES
+    (1, 1, 1, 1, GETDATE(), 1, 'Comentario 1'),
+    (2, 2, 2, 2, GETDATE(), 0, 'Comentario 2'),
+    (3, 3, 3, 3, GETDATE(), 1, 'Comentario 3'),
+    (4, 4, 4, 4, GETDATE(), 0, 'Comentario 4'),
+    (5, 5, 1, 5, GETDATE(), 1, 'Comentario 5'),
+    (6, 6, 2, 6, GETDATE(), 0, 'Comentario 6'),
+    (7, 7, 3, 7, GETDATE(), 1, 'Comentario 7'),
+    (8, 8, 4, 8, GETDATE(), 0, 'Comentario 8'),
+    (9, 9, 1, 9, GETDATE(), 1, 'Comentario 9'),
+    (10, 10, 2, 10, GETDATE(), 0, 'Comentario 10');
+
+INSERT INTO Tecnico (Nombre, Contacto, FechaIncorporacion, Estado)
+VALUES
+    ('Técnico1', '123-456-7890', GETDATE(), 1),
+    ('Técnico2', '987-654-3210', GETDATE(), 1),
+    ('Técnico3', '555-555-5555', GETDATE(), 1);
+
+INSERT INTO TipoMantenimiento (Nombre)
+VALUES
+    ('Mantenimiento Preventivo'),
+    ('Mantenimiento Correctivo'),
+    ('Mantenimiento Predictivo'),
+    ('Mantenimiento de Emergencia');
+
+INSERT INTO Mantenimiento (IdServicio, Fecha, IdTecnico, Descripcion, IdTipoMantenimiento, Comentarios, EstadoRealizacion)
+VALUES
+    (1, GETDATE(), 1, 'Mantenimiento 1', 1, 'Comentario Mantenimiento 1', 1),
+    (2, GETDATE(), 2, 'Mantenimiento 2', 2, 'Comentario Mantenimiento 2', 0),
+    (3, GETDATE(), 1, 'Mantenimiento 3', 3, 'Comentario Mantenimiento 3', 1),
+    (4, GETDATE(), 3, 'Mantenimiento 4', 4, 'Comentario Mantenimiento 4', 0),
+    (5, GETDATE(), 2, 'Mantenimiento 5', 1, 'Comentario Mantenimiento 5', 1),
+    (6, GETDATE(), 1, 'Mantenimiento 6', 2, 'Comentario Mantenimiento 6', 0),
+    (7, GETDATE(), 3, 'Mantenimiento 7', 3, 'Comentario Mantenimiento 7', 1),
+    (8, GETDATE(), 2, 'Mantenimiento 8', 4, 'Comentario Mantenimiento 8', 0),
+    (9, GETDATE(), 1, 'Mantenimiento 9', 1, 'Comentario Mantenimiento 9', 1),
+    (10, GETDATE(), 3, 'Mantenimiento 10', 2, 'Comentario Mantenimiento 10', 0);
