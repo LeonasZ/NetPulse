9c708021dfb6d33cdaef5edcc08848b2a8e61604
Teo Beltran <teo.belnan@gmail.com>
2023-11-11 21:42:48 -0300

    Arreglado

---
+++ NetPulse/Tecnicos.aspx.cs
@@ -0,0 +1,20 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NetPulse
+{
+    public partial class Tecnicos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TecnicoNegocio TecnicoNegocio = new TecnicoNegocio();
+            dgvListaTecnicos.DataSource = TecnicoNegocio.listarTecnicos();
+            dgvListaTecnicos.DataBind();
+        }
+    }
+}
